25148f5a7442180123a07603e51fb60bfa7ce1e1
okeyemi <okeyemitunde>
2022-08-25 14:05:38 +0100

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<AppHeader />
+<Calendar />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from "./components/AppHeader";
+import Calendar from "./components/Calendar";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+export default{
+  components: {AppHeader, Calendar},
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
---
+++ src/components/pages/Markdown.vue
@@ -0,0 +1,16 @@
+<template>
+<section>
+    <article>left</article>
+    <article>right</article>
+</section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
